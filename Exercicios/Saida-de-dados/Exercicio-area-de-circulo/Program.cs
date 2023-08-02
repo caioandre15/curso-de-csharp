bdff55cb164cbf2221751eb9e8a81a6f66672d01
CAIO ANDRE <caio.andre@juntossomosmais.com.br>
2023-08-02 08:17:58 -0300

    Adicionando exercicios, area de um circulo e diferença de um produto.

---
+++ Exercicios/Saida-de-dados/Exercicio-area-de-circulo/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Globalization;
+
+double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double pi = 3.14159;
+double area = pi * Math.Pow(raio, 2.0);
+
+Console.WriteLine($"A={area.ToString("F4", CultureInfo.InvariantCulture)}");
