003553d02aab79e9cd7787cf10618897e83b691f
Caio André <caio_andre15@hotmail.com>
2023-08-17 10:27:50 -0300

    Adding exercise.

---
+++ Exercicios/Repeticao/Exer-02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+
+var coordenadas = Console.ReadLine().Split(" ");
+
+var x = Double.Parse(coordenadas[0]);
+var y = Double.Parse(coordenadas[1]);
+
+while (x != 0 && y != 0)
+{
+    if (x > 0)
+    {
+        if (y > 0)
+            Console.WriteLine("primeiro");
+        else
+            Console.WriteLine("quarto");
+    }
+    if (x < 0)
+    {
+        if (y > 0)
+            Console.WriteLine("segundo");
+        else
+            Console.WriteLine("terceiro");
+    }
+
+    coordenadas = Console.ReadLine().Split(" ");
+
+    x = Double.Parse(coordenadas[0]);
+    y = Double.Parse(coordenadas[1]);
+}
+
+
