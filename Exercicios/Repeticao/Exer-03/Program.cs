fe38ac8905409d38a7a7c3cb18f3152fc7745812
Caio André <caio_andre15@hotmail.com>
2023-08-18 06:44:52 -0300

    adding exercise

---
+++ Exercicios/Repeticao/Exer-03/Program.cs
@@ -0,0 +1,22 @@
+﻿var codigo = int.Parse(Console.ReadLine());
+
+var qtdeAlcool = 0;
+var qtdeGasolina = 0;
+var qtdeDiesel = 0;
+
+while (codigo != 4)
+{
+    if (codigo == 1)
+        qtdeAlcool++;
+    if (codigo == 2)
+        qtdeGasolina++;
+    if (codigo == 3)
+        qtdeDiesel++;
+
+    codigo = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("MUITO OBRIGADO");
+Console.WriteLine("Alcool: " + qtdeAlcool);
+Console.WriteLine("Gasolina: " + qtdeGasolina);
+Console.WriteLine("Diesel: " + qtdeDiesel);
