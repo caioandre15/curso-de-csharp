7e3edfce035130eaf80ea54ff48cf8cb56408f04
Caio André <caio_andre15@hotmail.com>
2023-08-18 07:37:39 -0300

    adding exercise

---
+++ Exercicios/Repeticao/Exer-05/Program.cs
@@ -0,0 +1,15 @@
+﻿var n = int.Parse(Console.ReadLine());
+var qdteIn = 0;
+var qdteOut = 0;
+
+for (int i = 1; i<= n; i++)
+{
+    var valor = int.Parse(Console.ReadLine());
+    if (valor >= 10 && valor <= 20)
+        qdteIn++;
+    if (valor < 10 || valor > 20)
+        qdteOut++;
+}
+
+Console.WriteLine(qdteIn + " in");
+Console.WriteLine(qdteOut + " out");
