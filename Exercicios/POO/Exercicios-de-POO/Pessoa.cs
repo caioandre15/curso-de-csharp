d3d15cc662f2934ca9249f21620d47bfd98f54c3
Caio André <caio_andre15@hotmail.com>
2023-08-22 07:33:53 -0300

    feat: adding exercise.

---
+++ Exercicios/POO/Exercicios-de-POO/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicios_de_POO
+{
+    public class Pessoa
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public static string pessoaMaisVelha(Pessoa p1, Pessoa p2)
+        {
+            if (p1.Age > p2.Age)
+                return p1.Name;
+            else
+                return p2.Name;
+        }
+    }
+}
---
+++ Exercicios/POO/Exercicios-de-POO/Program.cs
@@ -0,0 +1,18 @@
+﻿using Exercicios_de_POO;
+
+Pessoa pessoa1 = new Pessoa();
+Pessoa pessoa2 = new Pessoa();
+
+Console.WriteLine("Dados da primeira pessoa: ");
+Console.Write("Nome: ");
+pessoa1.Name = Console.ReadLine();
+Console.Write("Idade: ");
+pessoa1.Age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados da segunda pessoa: ");
+Console.Write("Nome: ");
+pessoa2.Name = Console.ReadLine();
+Console.Write("Idade: ");
+pessoa2.Age = int.Parse(Console.ReadLine());
+
+Console.Write("Pessoa mais velha: " + Pessoa.pessoaMaisVelha(pessoa1, pessoa2));
