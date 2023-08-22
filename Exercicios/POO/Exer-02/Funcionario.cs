1abce5b7b84281b3182e0215b2c575ccab62e075
Caio André <caio_andre15@hotmail.com>
2023-08-22 07:54:26 -0300

    feat: adding exercises.

---
+++ Exercicios/POO/Exer-02/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer_02
+{
+    public class Funcionario
+    {
+        public string Name { get; set; }
+        public double Salario { get; set; }
+        
+        public static double SalarioMedio(Funcionario f1, Funcionario f2)
+        {
+            return (f1.Salario + f2.Salario) / 2; 
+        }
+
+    }
+}
---
+++ Exercicios/POO/Exer-02/Program.cs
@@ -0,0 +1,24 @@
+﻿using Exer_02;
+using System.Globalization;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Funcionario funcionario1 = new Funcionario();
+        Funcionario funcionario2 = new Funcionario();
+
+        Console.WriteLine("Dados do primeiro funcionário: ");
+        Console.Write("Nome: ");
+        funcionario1.Name = Console.ReadLine();
+        Console.Write("Salário: ");
+        funcionario1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Dados do segundo funcionário: ");
+        Console.Write("Nome: ");
+        funcionario2.Name = Console.ReadLine();
+        Console.Write("Salário: ");
+        funcionario2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Salário Médio = " + Funcionario.SalarioMedio(funcionario1, funcionario2).ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
